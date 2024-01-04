98b21389343bbbc3f802f27afd1fcdb1dade21fd
Your Name <furkantural3328@hotmail.com>
2024-01-04 17:14:52 +0300

    customer spawner added

---
+++ Assets/Scripts/Entitys/CustomerSpawner.cs
@@ -0,0 +1,45 @@
+﻿using Events;
+using GenericPoolSystem;
+using NaughtyAttributes;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Entitys
+{
+    public class CustomerSpawner : MonoBehaviour
+    {
+        [SerializeField] private List<Transform> wayPoints;
+
+        private int _listCount;
+
+        private void Awake()
+        {
+            _listCount = wayPoints.Count;
+        }
+
+        [Button]
+        public void Spawn()
+        {
+            Transform spawnPoint = GetRandomWayPoint();
+
+            GameObject customer = PoolSignals.onGetObjectFormPool("Customer");
+            customer.transform.position = spawnPoint.position;
+
+            Customer _customer = customer.GetComponent<Customer>();
+            _customer.SpawnPoint = spawnPoint;
+            _customer.ReStartStateMachine();
+        }
+
+        private Transform GetRandomWayPoint() => wayPoints[UnityEngine.Random.Range(0, _listCount)];
+
+        private void OnEnable()
+        {
+            SpawnEvent.OnSpawn += Spawn;
+        }
+
+        private void OnDisable()
+        {
+            SpawnEvent.OnSpawn -= Spawn;
+        }
+    }
+}
